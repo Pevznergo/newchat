b725fda4cf09c7a38d9ee965acfbd9d67bf00314
Pevznergo <102965261+Pevznergo@users.noreply.github.com>
2026-01-27 15:11:11 +0300

    Create layout.tsx

---
+++ app/clan/layout.tsx
@@ -0,0 +1,17 @@
+import Script from "next/script";
+
+export default function ClanLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="min-h-screen bg-[#1c1c1e] text-white">
+      <Script
+        src="https://telegram.org/js/telegram-web-app.js"
+        strategy="beforeInteractive"
+      />
+      {children}
+    </div>
+  );
+}
